a63e6794382286382d73b4ff386dd6d857cba8ac
Thomas Müller <thomas.mueller@tmit.eu>
2015-03-31 11:13:46 +0200

    adding function getSecureRandom(); to IServerContainer

--- lib/private/server.php
+++ lib/private/server.php
@@ -688,7 +688,7 @@ class Server extends SimpleContainer implements IServerContainer {
 	 *
 	 * @return \OCP\Security\ISecureRandom
 	 */
-function getSecureRandom() {
+	function getSecureRandom() {
 		return $this->query('SecureRandom');
 	}
 
